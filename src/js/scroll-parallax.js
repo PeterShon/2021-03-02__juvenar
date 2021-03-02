78e56a1037cc1c207c5ece06aca00f78c0530363
PeterShon <petershonpublic@gmail.com>
2021-03-02 14:58:28 +0300

    first commit

---
+++ src/js/scroll-parallax.js
@@ -0,0 +1,20 @@
+/* scroll-parallax effect */
+export function scrollParallax() {
+  let Rellax = require('rellax')
+  var rocket1 = new Rellax('.scroll-parallax__item_1', {
+    center: true,
+    speed: 2
+  });
+  var rocket2 = new Rellax('.scroll-parallax__item_2', {
+    center: true,
+    speed: 5
+  });
+  var rocket3 = new Rellax('.scroll-parallax__item_3', {
+    center: true,
+    speed: 10
+  });
+  var planet = new Rellax('.scroll-parallax__item_4', {
+    center: true,
+    speed: -4
+  });
+}
